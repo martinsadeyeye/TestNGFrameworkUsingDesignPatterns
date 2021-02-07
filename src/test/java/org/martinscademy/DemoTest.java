f3432c105cfe1f1c5fc5376c84f1ca2b3f9555a0
Adeyeye <adeyeye@systemspecs.com.ng>
2021-02-07 12:04:56 +0100

    Parameterzaton adding Data provider for executing tests

--- src/test/java/org/martinscademy/DemoTest.java
+++ src/test/java/org/martinscademy/DemoTest.java
@@ -3,14 +3,15 @@ package org.martinscademy;
 import org.martinscademy.PageObjects.TravelHomePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
 
 public class DemoTest {
 
-    @Test
-    public void flightTest() {
+    @Test(dataProvider = "getData")
+    public void flightTest(HashMap<String, String> reservationDetails) {
 
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\MARTINS\\Downloads\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
@@ -23,7 +24,12 @@ public class DemoTest {
         System.out.println(travelHomePage.getNavigationBar().getLinkCount());
 
         //travelHomePage.setBookingStrategy(new RoundTrip(driver,sectionElement));// Factory Design Patter solve this issue of creating object in a test class
-        travelHomePage.setBookingStrategy("multitrip");//roundtrip
+        travelHomePage.setBookingStrategy("roundtrip");//roundtrip //multitrip
+
+    }
+
+    @DataProvider
+    public Object[][] getData() {
         HashMap<String, String> reservationDetails = new HashMap<String, String>();
         reservationDetails.put("origin", "MAA");
         reservationDetails.put("destination", "HYD");
@@ -31,7 +37,19 @@ public class DemoTest {
         reservationDetails.put("destination2", "GOI");
         reservationDetails.put("origin3", "JLR");
         reservationDetails.put("destination3", "DEL");
-        travelHomePage.checkAvail(reservationDetails);
 
+        HashMap<String, String> reservationDetails1 = new HashMap<String, String>();
+        reservationDetails1.put("origin", "MAA");
+        reservationDetails1.put("destination", "HYD");
+        reservationDetails1.put("origin2", "BLR");
+        reservationDetails1.put("destination2", "GOI");
+        reservationDetails1.put("origin3", "JLR");
+        reservationDetails1.put("destination3", "DEL");
+
+        return new Object[][]
+                {
+                        {reservationDetails}, {reservationDetails}
+                };
     }
+
 }
